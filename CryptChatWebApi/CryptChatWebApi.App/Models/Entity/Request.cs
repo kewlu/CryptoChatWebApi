c850be6e0b4761dde725441e098a73fa98a72e8f
kewlu <pampam@my.com>
2019-07-07 19:57:32 +0300

    add AdminController

---
+++ CryptChatWebApi/CryptChatWebApi.App/Models/Entity/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptChatWebApi.App.Models.Entity
+{
+    public class Request<T>
+    {
+        public int requestId { get; set; }
+
+        public T data { get; set; }
+    }
+}
--- CryptChatWebApi/CryptChatWebApi.App/Models/Entity/User.cs
+++ CryptChatWebApi/CryptChatWebApi.App/Models/Entity/User.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CryptChatWebApi.App.Models.Entity
 {
+    public enum colour
+    {
+        black,
+        white,
+        red
+
+    }
+
     public class User
     {
         public int UserId { get; set; }
-
+        [Required]
         public string Login { get; set; }
-
+        [Required]
         public string Password { get; set; }
+
+        public string Nickname { get; set; }
+
+        public colour Color { get; set; } = 0;
+
+        public int rank { get; set; } = 0;
+
+        public int timestamp { get; set; } = 0;
     }
 }
